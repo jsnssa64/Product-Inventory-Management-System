88254b1267dcf5f6f0871e425b301c2579f07e1a
JSDance <jsnsousa92@hotmail.com>
2021-12-08 17:16:37 +0000

    Create ProductOrder Query DTO and remove ValueObject
    Product order is a very popular dto and ValueObject is no longer necessary

---
+++ Application/ProductOrder/Queries/Dtos/ProductOrderDetailsDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ProductOrder.Queries.Dtos
+{
+    public class ProductOrder
+    {
+        public Guid Id { get; set; }
+        public string Name;
+        public int CurrentCount;
+        public int Version;
+
+        public ProductOrder(Guid id, string name, int currentCount, int version)
+        {
+            Id = id;
+            Name = name;
+            CurrentCount = currentCount;
+            Version = version;
+        }
+    }
+}
