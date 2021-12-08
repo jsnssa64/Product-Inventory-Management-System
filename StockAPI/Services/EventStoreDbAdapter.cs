9518e2f11331d098625c37fb3615a6d0189b68dd
JSDance <jsnsousa92@hotmail.com>
2021-12-08 17:23:32 +0000

    Create an Adapter for CQRS lite
    CQRS lite uses IEventStore for handling event sourcing storage, tryin to see if an adapter is appropriate.

---
+++ StockAPI/Services/EventStoreDbAdapter.cs
@@ -0,0 +1,31 @@
+﻿using CQRSlite.Events;
+using EventStore.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StockAPI.Services
+{
+    public class EventStoreDbAdapter : IEventStore
+    {
+        public readonly EventStoreClient _client;
+        public EventStoreDbAdapter(EventStoreClient client)
+        {
+            _client = client;
+        }
+        public Task<IEnumerable<IEvent>> Get(Guid aggregateId, int fromVersion, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(_context.EventStorage.AsEnumerable().Where(x => x.StreamId == aggregateId && x.Version > fromVersion).Select(s => JsonSerializer.Deserialize<IEvent>(s.Payload)));
+
+            _client.
+            throw new NotImplementedException();
+        }
+
+        public Task Save(IEnumerable<IEvent> events, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
