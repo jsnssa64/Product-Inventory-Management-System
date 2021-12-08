c228a7d34da4d7ad0eddec85dc8d265df3d76135
JSDance <jsnsousa92@hotmail.com>
2021-12-08 17:24:44 +0000

    Create Template Background Service for testing
    Simply designing for reference background service to consider for microservices and masstransit with RabbitMQ in future

---
+++ StockAPI/Workers/BusHostedService.cs
@@ -0,0 +1,30 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StockAPI.Workers
+{
+    public class BusHostedService : BackgroundService
+    {
+        readonly IBus _bus;
+
+        public BusHostedService(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await _bus.Publish(new Message { Text = $"The time is {DateTimeOffset.Now}" });
+
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
