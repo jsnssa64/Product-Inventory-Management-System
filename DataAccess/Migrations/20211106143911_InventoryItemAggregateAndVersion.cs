2e7118277cad5f1854b92e442f22d9d07255e36f
JSDance <jsnsousa92@hotmail.com>
2021-12-08 17:01:15 +0000

    Add Event Sourcing Controls
    This is most likely to change to EventSourceDB, so this is only a temporary change to be updated very soon

---
+++ DataAccess/Migrations/20211106143911_InventoryItemAggregateAndVersion.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class InventoryItemAggregateAndVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AggregateId",
+                table: "InventoryItem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "InventoryItem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AggregateId",
+                table: "InventoryItem");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "InventoryItem");
+        }
+    }
+}
--- DataAccess/Model/Stock/Inventory.cs
+++ DataAccess/Model/Stock/Inventory.cs
@@ -1,17 +1,22 @@
 ﻿using Data.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data.Model
 {
-    public class Inventory
+    public class InventoryItem
     {
         public int Id { get; set; }
         public int ProductItemId { get; set; }
         public int UnitsInStock { get; set; }
         public ProductItem ProductItem { get; set; }
+        public bool Deactivated { get; set; } 
+        //  CQRS - ES
+        public int AggregateId { get; set; }
+        public int Version { get; set; }
     }
 }
