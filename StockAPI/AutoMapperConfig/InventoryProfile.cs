2dfa7e16b1f690f71e15b60aed9cd68459c733fb
JSDance <jsnsousa92@hotmail.com>
2021-12-08 17:12:18 +0000

    Map Inventory to Inventory Events

---
+++ StockAPI/AutoMapperConfig/InventoryProfile.cs
@@ -0,0 +1,26 @@
+﻿using Application.Inventory.Queries.Dtos;
+using Application.Inventory.WriteModel.Commands;
+using AutoMapper;
+using Data.Model;
+using Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockAPI.AutoMapperConfig
+{
+    public class InventoryProfile : Profile
+    {
+        public InventoryProfile()
+        {
+            //  Frontend -> CommandHandler
+            CreateMap<InventoryItemDto, CreateInventoryItemCommand>().ConstructUsing(x => new CreateInventoryItemCommand(Guid.NewGuid(), 0, x.ProductItemId, x.UnitsInStock));
+            //  Event -> Persistence
+            CreateMap<CreateItemInInventoryEvent, InventoryItem>()  .ForMember(dest => dest.Id, opt => opt.Ignore())
+                                                                    .ForMember(dest => dest.AggregateId, opt => opt.MapFrom(src => src.Id));
+            CreateMap<ItemRemovedFromInventoryEvent, InventoryItem>().ForMember(dest => dest.AggregateId, opt => opt.MapFrom(src => src.Id));
+            
+        }
+    }
+}
