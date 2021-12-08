46e5dd6271bee5b881df5093b71a2fca4b1465f7
JSDance <jsnsousa92@hotmail.com>
2021-12-08 17:22:06 +0000

    Create Basic Logging Middleware
    Just storing a basic logging to then start working on middleware in the future

---
+++ StockAPI/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockAPI.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class RequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public RequestLoggingMiddleware(RequestDelegate next, ILoggerFactory loggerFactory) // Dependency Inject as usual
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<RequestLoggingMiddleware>();
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+
+            try
+            {
+                await _next(httpContext);
+            }
+            finally
+            {
+                _logger.LogInformation(
+                    "Request {method} {url} => {statusCode}",
+                    httpContext.Request?.Method,
+                    httpContext.Request?.Path.Value,
+                    httpContext.Response?.StatusCode);
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class RequestLoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestLoggingMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestLoggingMiddleware>();
+        }
+    }
+}
