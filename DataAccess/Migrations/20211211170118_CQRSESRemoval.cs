3cbe9640761c2d14da6f6cb0b94113f57ccf243b
JSDance <jsnsousa92@hotmail.com>
2021-12-17 09:53:19 +0000

    Database Migration Files for the new Product and Inventory Updates

---
+++ DataAccess/Migrations/20211211170118_CQRSESRemoval.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class CQRSESRemoval : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AggregateId",
+                table: "InventoryItem");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "InventoryItem");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Deactivated",
+                table: "InventoryItem",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Deactivated",
+                table: "InventoryItem");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AggregateId",
+                table: "InventoryItem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "InventoryItem",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
---
+++ DataAccess/Migrations/20211213211816_AddDiscontinuationProducts.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class AddDiscontinuationProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfDiscontinuedUTC",
+                table: "Products",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Discontinued",
+                table: "Products",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfDiscontinuedUTC",
+                table: "ProductItem",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Discontinued",
+                table: "ProductItem",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfDiscontinuedUTC",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Discontinued",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfDiscontinuedUTC",
+                table: "ProductItem");
+
+            migrationBuilder.DropColumn(
+                name: "Discontinued",
+                table: "ProductItem");
+        }
+    }
+}
