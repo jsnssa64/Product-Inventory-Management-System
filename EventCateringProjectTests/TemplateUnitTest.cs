677f2a9d6d1f661334d72eb628b91bf775ca8345
JSDance <jsnsousa92@hotmail.com>
2021-12-17 09:50:55 +0000

    NUnit Basic Template Addition

--- EventCateringProjectTests/TemplateUnitTest.cs
+++ EventCateringProjectTests/TemplateUnitTest.cs
@@ -1,18 +1,46 @@
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace EventCateringProjectTests
 {
+    [TestFixture]
     public class Tests
     {
         [SetUp]
         public void Setup()
         {
+
         }
 
+
         [Test]
-        public void Test1()
+        [TestCaseSource(nameof(DataGenerator), new Object[] { 3 })]
+        [TestCase(1, TestName ="ValueOfOne", Author ="Jason")]
+        [TestCase(2, TestName ="ValueOfTwo")]
+        [TestCase(3, TestName ="ValueOfThree", IgnoreReason = "Because this will make all the testcases fail")]
+        public void Test1(int a)
         {
-            Assert.Pass();
+            //  ARRANGE
+            //  ACT
+            int value = a;
+            //  ASSERT
+            Assert.That(value, Is.Not.EqualTo(3));
+        }
+
+        static IEnumerable<int> DataGenerator(int counter) {
+            while(counter > 0)
+            {
+                Console.WriteLine(counter);
+                yield return counter;
+                counter--;
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Console.WriteLine("Tear Down Test");
         }
     }
 }
